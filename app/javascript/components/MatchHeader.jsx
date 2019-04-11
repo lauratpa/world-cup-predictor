4eee189482546c634238e098c8df2895e8d2f256
Laura Paakkinen <la.paakkinen@gmail.com>
2019-04-11 22:17:30 +0200

    Make kick off counter dynamic

--- app/javascript/components/MatchHeader.jsx
+++ app/javascript/components/MatchHeader.jsx
@@ -3,9 +3,39 @@ import PropTypes from "prop-types";
 import { Card } from "semantic-ui-react";
 import moment from "moment";
 
-const MatchHeader = ({ kickOff }) => (
-  <Card.Header textAlign="center">{moment(kickOff).format("LLL")}</Card.Header>
-);
+class MatchHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    const { kickOff } = props;
+
+    this.state = { fromNow: moment(kickOff).fromNow() };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    const { kickOff } = this.props;
+
+    this.setState({ fromNow: moment(kickOff).fromNow() });
+  }
+
+  render() {
+    const { kickOff } = this.props;
+    const { fromNow } = this.state;
+
+    return (
+      <Card.Header textAlign="center">
+        {moment(kickOff).format("LLL")} - {fromNow}
+      </Card.Header>
+    );
+  }
+}
 
 MatchHeader.propTypes = {
   kickOff: PropTypes.string.isRequired
